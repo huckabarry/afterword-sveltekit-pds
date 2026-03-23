9977650814d3dc7bea53d8f3b8451c9aefb15091
huckabarry <bryan.robb@gmail.com>
2026-03-23 09:17:34 -0700

    Filter blog index to planning and field notes

--- src/routes/blog/+page.server.ts
+++ src/routes/blog/+page.server.ts
@@ -2,8 +2,20 @@ import { getBlogPosts } from '$lib/server/ghost';
 
 export const prerender = false;
 
+const BLOG_INDEX_TAGS = new Set([
+	'field-notes',
+	'gallery',
+	'photography',
+	'urbanism',
+	'housing',
+	'transportation',
+	'public-finance'
+]);
+
 export async function load() {
+	const posts = await getBlogPosts();
+
 	return {
-		posts: await getBlogPosts()
+		posts: posts.filter((post) => post.publicTags.some((tag) => BLOG_INDEX_TAGS.has(tag.slug)))
 	};
 }
