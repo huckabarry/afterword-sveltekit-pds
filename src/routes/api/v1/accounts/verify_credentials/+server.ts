0647a83b172684b694e843aacf328a01f48e4cb5
huckabarry <bryan.robb@gmail.com>
2026-03-23 00:46:14 -0700

    Revert "Add minimal Mastodon API compatibility"
    This reverts commit 1fa9bfbb7157e22b018e5703726683c71def2ee5.

--- src/routes/api/v1/accounts/verify_credentials/+server.ts
+++
@@ -1,8 +0,0 @@
-import { json } from '@sveltejs/kit';
-import { requireAccessToken } from '$lib/server/mastodon-auth';
-import { getInstanceOrigin, getMastodonAccount } from '$lib/server/mastodon-api';
-
-export async function GET(event) {
-	await requireAccessToken(event);
-	return json(getMastodonAccount(getInstanceOrigin(event.url)));
-}
