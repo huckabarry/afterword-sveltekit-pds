2f1c3a27cfe09621cc13a5f8075028ceba4255cf
huckabarry <bryan.robb@gmail.com>
2026-03-29 01:18:27 -0700

    Refine gallery intro copy

--- src/routes/photos/+page.svelte
+++ src/routes/photos/+page.svelte
@@ -45,7 +45,7 @@
 	<h1 class="stream-head__title">Visual Notes</h1>
 	<div class="stream-head__lede">
 		<p>
-			Mostly photos of places I've been, street photography, and landscapes. Photography is just a hobby of mine, something I'm still experimenting with and trying to figure out. It's an outlet like any other thing.
+			Most of these are photos of places I've visited, street scenes, and landscapes. Photography is just a hobby for me, and I'm still learning and trying new things. It's just another way for me to express myself.
 		</p>
 	</div>
 </section>
