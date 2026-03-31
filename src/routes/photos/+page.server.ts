976a5a206acecda59bcc570198a5bb72ff8c9bc5
huckabarry <bryan.robb@gmail.com>
2026-03-30 17:56:46 -0700

    Make Ghost the source of truth for gallery discovery

--- src/routes/photos/+page.server.ts
+++ src/routes/photos/+page.server.ts
@@ -1,21 +1,21 @@
-import { attachGalleryAssetUrls } from '$lib/server/gallery-assets';
+import { attachGalleryAssetUrls, type GalleryPhotoItem } from '$lib/server/gallery-assets';
 import { getPhotoItems } from '$lib/server/ghost';
 import { getManifestGalleryPhotos } from '$lib/server/photo-manifest';
 
 export const prerender = false;
 
 export async function load(event) {
-	const manifestPhotos = await getManifestGalleryPhotos(event);
-
-	if (manifestPhotos.length) {
-		return {
-			photos: manifestPhotos
-		};
-	}
-
 	const photos = await getPhotoItems();
+	const localizedPhotos = await attachGalleryAssetUrls(photos, event.platform?.env.R2_BUCKET);
+	const manifestPhotos = await getManifestGalleryPhotos(event);
+	const manifestById = new Map(
+		manifestPhotos.map((photo: GalleryPhotoItem) => [photo.id, photo] as const)
+	);
 
 	return {
-		photos: await attachGalleryAssetUrls(photos, event.platform?.env.R2_BUCKET)
+		photos: localizedPhotos.map((photo) => ({
+			...photo,
+			...(manifestById.get(photo.id) || {})
+		}))
 	};
 }
