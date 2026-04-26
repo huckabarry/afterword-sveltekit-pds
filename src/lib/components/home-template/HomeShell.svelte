15c64808b9a36e747c8ec9dbde414ea97d815ce1
huckabarry <bryan.robb@gmail.com>
2026-04-25 22:25:47 -0700

    Revert "Make theme toggle independent of hydration"
    This reverts commit 645962768d111be4ae5346624c4dbb42e3c26a79.

--- src/lib/components/home-template/HomeShell.svelte
+++ src/lib/components/home-template/HomeShell.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
+
 	let {
 		title,
 		wide = false,
@@ -8,6 +10,40 @@
 		wide?: boolean;
 		children?: import('svelte').Snippet;
 	} = $props();
+
+	let isDarkMode = $state(true);
+
+	function disableTransitionsTemporarily() {
+		if (!browser) return;
+		document.documentElement.classList.add('template-home-disable-transitions');
+		window.setTimeout(() => {
+			document.documentElement.classList.remove('template-home-disable-transitions');
+		}, 0);
+	}
+
+	function applyTheme(nextDarkMode: boolean) {
+		if (!browser) return;
+
+		isDarkMode = nextDarkMode;
+		localStorage.setItem('templateHomeIsDarkMode', nextDarkMode.toString());
+		disableTransitionsTemporarily();
+
+		if (nextDarkMode) {
+			document.documentElement.classList.add('dark');
+		} else {
+			document.documentElement.classList.remove('dark');
+		}
+	}
+
+	$effect(() => {
+		if (!browser) return;
+
+		const saved = localStorage.getItem('templateHomeIsDarkMode');
+		const nextDarkMode =
+			saved === 'true' || (saved === null && document.documentElement.classList.contains('dark'));
+
+		applyTheme(nextDarkMode);
+	});
 </script>
 
 <div class="template-shell">
@@ -21,22 +57,25 @@
 				type="button"
 				role="switch"
 				aria-label="Toggle Dark Mode"
-				aria-checked="true"
-				data-theme-toggle
+				aria-checked={isDarkMode}
 				class="template-shell__theme-toggle"
+				onclick={() => applyTheme(!isDarkMode)}
 			>
-				<svg viewBox="0 0 24 24" aria-hidden="true" class="template-shell__theme-icon template-shell__theme-icon--moon">
-					<path
-						d="M17.75 15.5a.75.75 0 0 1 .67 1.09A8.25 8.25 0 1 1 7.41 5.58a.75.75 0 0 1 1.09.67 7 7 0 0 0 9.25 9.25Z"
-						fill="currentColor"
-					/>
-				</svg>
-				<svg viewBox="0 0 24 24" aria-hidden="true" class="template-shell__theme-icon template-shell__theme-icon--sun">
-					<path
-						d="M12 4.75a.75.75 0 0 1 .75.75v1.5a.75.75 0 0 1-1.5 0V5.5a.75.75 0 0 1 .75-.75Zm0 11.5a.75.75 0 0 1 .75.75v1.5a.75.75 0 0 1-1.5 0V17a.75.75 0 0 1 .75-.75Zm7.25-5a.75.75 0 0 1 0 1.5h-1.5a.75.75 0 0 1 0-1.5h1.5Zm-12.5 0a.75.75 0 0 1 0 1.5h-1.5a.75.75 0 0 1 0-1.5h1.5Zm8.485-4.735a.75.75 0 0 1 1.06 0l1.06 1.06a.75.75 0 1 1-1.06 1.061l-1.06-1.06a.75.75 0 0 1 0-1.06Zm-8.545 8.545a.75.75 0 0 1 1.06 0l1.06 1.06a.75.75 0 1 1-1.06 1.061l-1.06-1.06a.75.75 0 0 1 0-1.06Zm9.605 1.06a.75.75 0 0 1 1.06-1.06l1.06 1.06a.75.75 0 1 1-1.06 1.061l-1.06-1.06Zm-8.545-8.545a.75.75 0 0 1 1.06-1.06l1.06 1.06A.75.75 0 0 1 8.27 8.636l-1.06-1.06ZM12 8.25a3.75 3.75 0 1 1 0 7.5 3.75 3.75 0 0 1 0-7.5Z"
-						fill="currentColor"
-					/>
-				</svg>
+				{#if isDarkMode}
+					<svg viewBox="0 0 24 24" aria-hidden="true" class="template-shell__theme-icon">
+						<path
+							d="M17.75 15.5a.75.75 0 0 1 .67 1.09A8.25 8.25 0 1 1 7.41 5.58a.75.75 0 0 1 1.09.67 7 7 0 0 0 9.25 9.25Z"
+							fill="currentColor"
+						/>
+					</svg>
+				{:else}
+					<svg viewBox="0 0 24 24" aria-hidden="true" class="template-shell__theme-icon">
+						<path
+							d="M12 4.75a.75.75 0 0 1 .75.75v1.5a.75.75 0 0 1-1.5 0V5.5a.75.75 0 0 1 .75-.75Zm0 11.5a.75.75 0 0 1 .75.75v1.5a.75.75 0 0 1-1.5 0V17a.75.75 0 0 1 .75-.75Zm7.25-5a.75.75 0 0 1 0 1.5h-1.5a.75.75 0 0 1 0-1.5h1.5Zm-12.5 0a.75.75 0 0 1 0 1.5h-1.5a.75.75 0 0 1 0-1.5h1.5Zm8.485-4.735a.75.75 0 0 1 1.06 0l1.06 1.06a.75.75 0 1 1-1.06 1.061l-1.06-1.06a.75.75 0 0 1 0-1.06Zm-8.545 8.545a.75.75 0 0 1 1.06 0l1.06 1.06a.75.75 0 1 1-1.06 1.061l-1.06-1.06a.75.75 0 0 1 0-1.06Zm9.605 1.06a.75.75 0 0 1 1.06-1.06l1.06 1.06a.75.75 0 1 1-1.06 1.061l-1.06-1.06Zm-8.545-8.545a.75.75 0 0 1 1.06-1.06l1.06 1.06A.75.75 0 0 1 8.27 8.636l-1.06-1.06ZM12 8.25a3.75 3.75 0 1 1 0 7.5 3.75 3.75 0 0 1 0-7.5Z"
+							fill="currentColor"
+						/>
+					</svg>
+				{/if}
 			</button>
 		</header>
 
@@ -118,26 +157,6 @@
 	.template-shell__theme-icon {
 		width: 1.25rem;
 		height: 1.25rem;
-		display: block;
-	}
-
-	.template-shell__theme-icon--sun {
-		display: none;
-	}
-
-	:global(html.dark) .template-shell__theme-icon--moon {
-		display: block;
-	}
-
-	:global(html.dark) .template-shell__theme-icon--sun {
-		display: none;
-	}
-
-	:global(html:not(.dark)) .template-shell__theme-icon--moon {
-		display: none;
-	}
-
-	:global(html:not(.dark)) .template-shell__theme-icon--sun {
 		display: block;
 	}
 
