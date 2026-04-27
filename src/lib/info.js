a275459c30a0acbc92a2589a21c4518fd6f935aa
huckabarry <bryan.robb@gmail.com>
2026-04-26 20:22:58 -0700

    Refine home page byline copy

--- src/lib/info.js
+++ src/lib/info.js
@@ -6,7 +6,7 @@ export const authorName = `${firstName} ${lastName}`;
 export const name = 'Afterword';
 
 export const avatar = '/assets/images/status-avatar.jpg';
-export const bio = 'Urban planner and musician in the Pacific Northwest. Cities, photographs, books, and the usual preoccupations.';
+export const bio = 'I’m Bryan, an urban planner and musician in the Pacific Northwest. This is where I keep longer writing, field notes, and photographs, usually about cities, books, wandering around, and whatever else has been taking up space in my head lately.';
 export const email = 'bryan.robb@icloud.com';
 export const bluesky = 'https://bsky.app/profile/bryanrobb.bsky.social';
 
